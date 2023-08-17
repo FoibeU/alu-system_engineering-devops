d49c98f61f38adf0d6c863d2d785521f290febea
FoibeU <f.uwizeyima@alustudent.com>
2023-08-17 03:21:17 -0700

    last project

---
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Debug server error
+
+service {'nginx':
+    ensure => 'running'
+}
+
+exec { 'new-state':
+    command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g" /etc/default/nginx',
+    provider => 'shell',
+    notify   => service['nginx']
+}
